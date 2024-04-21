87e207afb2859f5f6db0c8861cb7a58694dff70e
chrislu03 <florecioayala03@gmail.com>
2024-04-21 13:57:49 -0400

    estilo al login

--- index.php
+++ index.php
@@ -1,27 +1,43 @@
-<?php
-if (!isset($_SESSION)){
-    header("Location: welcome.php");
-};  
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" href="src/css/style.css">
+    <title>Document</title>
 </head>
 <body>
-    <h2>Login</h2>
-    <h1>nuevo cambio</h1>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br>
-        <input type="checkbox" id="remember" name="remember">
-        <label for="remember">Remember me</label><br>
-        <input type="submit" value="Login">
-    </form>
-</body>
+    <div class="main">
+        <form class="form" action="login.php" method="POST">
+            <h1>Iniciar Sesion</h1>
+            <label class="label" for="username">Username</label>
+            <input class="input" type="text" name="username" placeholder="Username">
+
+            <label class="label" for="password">Password</label>
+            <input class="input" type="password" name="password" placeholder="Password">
+
+            <div class="group">
+                <a href="https://www.youtube.com/watch?v=mCdA4bJAGGk" target="_blank">Has olvidado tu contraseña</a>
+                <a href="">Registrarse</a>
+            </div>
+            <input class="sendbuttom" type="submit" value="Ingresar">
+
+            <p>Or contact with</p>
+
+            <div class="group-img">
+                <a href="https://www.instagram.com/ewar.dev/?hl=es-la" target="_blank">
+                    <img src="src/img/ewar.jpg">
+                </a>
+
+                <a href="https://www.instagram.com/christopher8304/?hl=es-la" target="_blank">
+                    <img src="src/img/sung.jpeg">
+                </a>
+            </div>
+        </form>
+
+
+
+    </div>
+
 </html>
+
